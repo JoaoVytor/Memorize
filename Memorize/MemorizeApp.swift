2db93cc0d06402b6c36a5aea5ba778da5d3d04ec
João Vitor Sousa <joaovitorsousa@Mac-mini-de-Joao.local>
2022-09-20 13:04:50 -0300

    Fist commit.

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by João Vitor Sousa on 20/09/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
